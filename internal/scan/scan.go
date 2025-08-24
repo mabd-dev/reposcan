3a51c4885a5a943e4be5c4001b6fa6052ad13bea
MABD <mabd.universe@gmail.com>
2025-08-24 11:35:19 +0300

    Added scanner to find git repos

---
+++ internal/scan/scan.go
@@ -0,0 +1,75 @@
+package scan
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// FindGitRepos walks each root and returns directories that look like Git worktrees.
+// Simple rules:
+// - A directory containing `.git` (directory) is a repo root.
+// - Or a `.git` file whose contents include "gitdir:" (worktrees/submodules).
+// - When we find a repo root, we SkipDir to avoid descending into nested repos (for now).
+func FindGitRepos(roots []string) []string {
+
+	gitRepos := []string{}
+
+	for _, root := range roots {
+		root = os.ExpandEnv(root)
+
+		_ = filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
+			if err != nil {
+				// TODO: return warnings back
+				// possible errors: permission denied
+				fmt.Println("Warning: " + err.Error())
+				return nil
+			}
+
+			if !d.IsDir() {
+				return nil
+			}
+
+			if isGitRepo(path) {
+				gitRepos = append(gitRepos, path)
+				return fs.SkipDir
+			}
+			return nil
+		})
+	}
+
+	return removeDuplicates(gitRepos)
+
+}
+
+func isGitRepo(path string) bool {
+	gitPath := filepath.Join(path, ".git")
+	if file, err := os.Lstat(gitPath); err == nil {
+		if file.IsDir() {
+			return true
+		} else {
+			// git worktrees has gitdir: folder
+			b, err := os.ReadFile(path)
+			if err != nil {
+				return false
+			}
+			return strings.Contains(string(b), "gitdir:")
+		}
+	}
+	return false
+}
+
+func removeDuplicates(strs []string) []string {
+	seen := make(map[string]struct{}, len(strs))
+	distinct := make([]string, 0, len(strs))
+
+	for _, s := range strs {
+		if _, ok := seen[s]; !ok {
+			seen[s] = struct{}{}
+			distinct = append(distinct, s)
+		}
+	}
+	return distinct
+}
