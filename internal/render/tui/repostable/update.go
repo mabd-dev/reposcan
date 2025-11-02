88d77862dc3570d99a135cb32926fb94ba22cfab
MABD <mabd.universe@gmail.com>
2025-11-02 22:22:52 +0200

    loop scroll in repos table

--- internal/render/tui/repostable/update.go
+++ internal/render/tui/repostable/update.go
@@ -2,9 +2,26 @@ package repostable
 
 import tea "github.com/charmbracelet/bubbletea"
 
-func (rt Model) Update(msg tea.Msg) (Model, tea.Cmd) {
-	var cmd tea.Cmd
-	rt.tbl, cmd = rt.tbl.Update(msg)
+func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "j":
+			if m.tbl.Cursor() == len(m.tbl.Rows())-1 {
+				m.tbl.SetCursor(0)
+				return m, nil
+			}
 
-	return rt, cmd
+		case "k":
+			if m.tbl.Cursor() == 0 {
+				m.tbl.SetCursor(len(m.tbl.Rows()) - 1)
+				return m, nil
+			}
+		}
+	}
+
+	var cmd tea.Cmd
+	m.tbl, cmd = m.tbl.Update(msg)
+
+	return m, cmd
 }
